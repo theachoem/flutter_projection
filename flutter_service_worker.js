20fff7f2baeede691c20c890099e54e4562e4d81
theachoem <29684683+theachoem@users.noreply.github.com>
2024-03-14 05:05:24 +0000

    Deploying to gh-pages from @ theachoem/flutter_projection@b7c17b1f9267c08bdff1b52ed02862db832cfd1e 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "363279e3710c2de90a1a3ae533df011d",
-"/": "363279e3710c2de90a1a3ae533df011d",
+const RESOURCES = {"index.html": "611c3a4596e2e18e7274f7617bde5959",
+"/": "611c3a4596e2e18e7274f7617bde5959",
 "canvaskit/canvaskit.js": "5caccb235fad20e9b72ea6da5a0094e6",
 "canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
 "canvaskit/skwasm.wasm": "d1fde2560be92c0b07ad9cf9acb10d05",
